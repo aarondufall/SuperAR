4032ef14b2864b33e8406180594c76576a8d282e
Aaron Dufall <aald212@gmail.com>
2014-01-31 07:57:37 -0800

    Load lib dir first

--- app.rb
+++ app.rb
@@ -3,11 +3,15 @@ require 'sqlite3'
 
 # Sets application root path to current path
 APP_ROOT = Pathname.new(File.dirname(File.expand_path(__FILE__)))
+Dir[APP_ROOT.join('lib','*.rb')].each { |file| require file }
 
 # Requires all *.rb file in the model folder
-Dir[APP_ROOT.join('model','*.rb')].each do |file| 
-	require file
-end 
+Dir[APP_ROOT.join('model','*.rb')].each { |file| require file }
+
+# Require all _migration.rb files in db/migration folder
+Dir[APP_ROOT.join('db','migrations','*_migration.rb')].each { |file| require file }
+
+
 
 # Sets sqlite3 database file to db/app.db
 # This will raise an error if the database has not yet been created
